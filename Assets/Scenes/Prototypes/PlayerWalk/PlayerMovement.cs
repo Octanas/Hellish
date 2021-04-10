65ec18b1fbd4d8db325dc58756a5a7ac25d2cb38
Filipe Barbosa <filipe.g.barbosa@gmail.com>
2021-04-10 17:52:26 +0100

    Add player walking test scene
    - Add Cinemachine

---
+++ Assets/Scenes/Prototypes/PlayerWalk/PlayerMovement.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Animator animator;
+    public Transform cameraTransform;
+
+    private float currentTurningVelocity;
+    public float turningTime = 0.1f;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        Vector3 inputMovement = new Vector3(Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical")).normalized;
+
+        animator.SetFloat("Movement", inputMovement.magnitude);
+
+        if (inputMovement.magnitude >= 0.1f)
+        {
+            float targetAngle = Mathf.Atan2(inputMovement.x, inputMovement.z) * Mathf.Rad2Deg + cameraTransform.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref currentTurningVelocity, turningTime);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            gameObject.transform.position = gameObject.transform.position + moveDir.normalized * (Input.GetButton("Sprint") ? 10 : 2) * Time.deltaTime;
+        }
+    }
+}
