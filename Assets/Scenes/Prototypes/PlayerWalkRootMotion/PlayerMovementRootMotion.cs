63d434ca6448897f564dba0e49d65fb907d2fe06
Filipe Barbosa <filipe.g.barbosa@gmail.com>
2021-04-12 16:28:59 +0100

    Add controller input support
    Install Input System

--- Assets/Scenes/Prototypes/PlayerWalkRootMotion/PlayerMovementRootMotion.cs
+++ Assets/Scenes/Prototypes/PlayerWalkRootMotion/PlayerMovementRootMotion.cs
@@ -1,14 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovementRootMotion : MonoBehaviour
 {
+    PlayerControls controls;
     private Animator animator;
     public Transform cameraTransform;
 
     private float currentTurningVelocity;
     public float turningTime = 0.1f;
+
+    private bool sprinting = false;
+    private Vector2 movementInput = Vector2.zero;
+
+    private void Awake()
+    {
+        controls = new PlayerControls();
+
+        controls.Gameplay.Sprint.performed += context => sprinting = true;
+        controls.Gameplay.Sprint.canceled += context => sprinting = false;
+
+        controls.Gameplay.Move.performed += context => movementInput = context.ReadValue<Vector2>();
+        controls.Gameplay.Move.canceled += context => movementInput = Vector2.zero;        
+    }
 
     private void Start()
     {
@@ -17,20 +33,26 @@ public class PlayerMovementRootMotion : MonoBehaviour
 
     void Update()
     {
-        Vector3 inputMovement = new Vector3(Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical")).normalized;
-
         float speed = 1;
 
-        if(Input.GetButton("Sprint"))
+        if (sprinting)
             speed = 10;
 
-        animator.SetFloat("Movement", inputMovement.magnitude * speed);
+        animator.SetFloat("Movement", movementInput.magnitude * speed);
 
-        if (inputMovement.magnitude >= 0.1f)
+        if (movementInput.magnitude >= 0.1f)
         {
-            float targetAngle = Mathf.Atan2(inputMovement.x, inputMovement.z) * Mathf.Rad2Deg + cameraTransform.eulerAngles.y;
+            float targetAngle = Mathf.Atan2(movementInput.x, movementInput.y) * Mathf.Rad2Deg + cameraTransform.eulerAngles.y;
             float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref currentTurningVelocity, turningTime);
             transform.rotation = Quaternion.Euler(0f, angle, 0f);
         }
+    }
+
+    private void OnEnable() {
+        controls.Gameplay.Enable();
+    }
+
+    private void OnDisable() {
+        controls.Gameplay.Disable();
     }
 }
