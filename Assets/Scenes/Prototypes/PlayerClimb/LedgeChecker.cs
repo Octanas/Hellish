86ebc3473ff3e5c527ff4966557c6775d445dec8
Filipe Barbosa <filipe.g.barbosa@gmail.com>
2021-04-18 00:15:22 +0100

    Comment climbing code

--- Assets/Scenes/Prototypes/PlayerClimb/LedgeChecker.cs
+++ Assets/Scenes/Prototypes/PlayerClimb/LedgeChecker.cs
@@ -1,10 +1,18 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// Handles detection of ledges for a character to latch on to.
+/// The character needs to have a single trigger collider,
+/// in the position where his hands will be when grabbing the ledge.
+/// This script should be added to the object containing that collider.
+/// </summary>
 public class LedgeChecker : MonoBehaviour
 {
     private Transform playerTransform;
+    /// <summary>
+    /// Player movement script.
+    /// </summary>
+    [Tooltip("Player movement script.")]
     public PlayerMovementClimb playerMovement;
 
     void Start()
--- Assets/Scenes/Prototypes/PlayerClimb/PlayerMovementClimb.cs
+++ Assets/Scenes/Prototypes/PlayerClimb/PlayerMovementClimb.cs
@@ -1,10 +1,18 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
+/// <summary>
+/// Manages player movement.
+/// </summary>
 public class PlayerMovementClimb : MonoBehaviour
 {
+    // STATE MANAGEMENT
+
+    /// <summary>
+    /// Player states (in accordance to the animations in the animator).
+    /// Each constant contains the hash value that represents each state.
+    /// These values should be compared with <see cref="AnimatorStateInfo.fullPathHash"/>.
+    /// </summary>
     public static class State
     {
         public static readonly int Moving = Animator.StringToHash("Base Layer.Moving");
@@ -12,6 +20,11 @@ public class PlayerMovementClimb : MonoBehaviour
         public static readonly int Climbing = Animator.StringToHash("Base Layer.Climbing");
     }
 
+    /// <summary>
+    /// Animator parameters (in accordance to the parameters set in the animator).
+    /// Each constant contains the string value of each parameter.
+    /// It does not contain information about the type of each parameter.
+    /// </summary>
     public static class AnimatorParameters
     {
         public static readonly string Movement = "Movement";
@@ -20,31 +33,46 @@ public class PlayerMovementClimb : MonoBehaviour
         public static readonly string ColliderHeight = "ColliderHeight";
     }
 
+    /// <summary>
+    /// Current state in the animator.
+    /// </summary>
     private AnimatorStateInfo state;
+    /// <summary>
+    /// Next state in the animator (if in a transition).
+    /// </summary>
     private AnimatorStateInfo nextState;
     private bool inStateTransition;
 
+    // EXTERNAL REFERENCES
     private PlayerControls controls;
     private Animator animator;
     private Rigidbody playerRigidbody;
     private CapsuleCollider playerCollider;
-    public Transform cameraTransform;
+    public Transform playerCamera;
+    /// <summary>
+    /// Reference point to where the hands will be when hanging on a ledge.
+    /// </summary>
+    [Tooltip("Reference point to where the hands will be when hanging on a ledge.")]
     public Transform hangingPoint;
 
+    // COLLIDER ADJUSTMENT
     private float defaultColliderHeight;
     private float colliderDiff = -1;
     private float originalColliderHeight = -1;
 
+    // CHARACTER TRANSLATION
     private bool translate = false;
     private Vector3 targetPosition;
     private Vector3 movingVelocity;
     public float movingTime = 0.1f;
     public float maxMovingVelocity = 10f;
 
+    // CHARACTER ROTATION
     private float targetAngle;
     private float turningVelocity;
     public float turningTime = 0.1f;
 
+    // MOVEMENT INPUT
     private Vector2 movementInput = Vector2.zero;
     private float movementInputSpeed = 0f;
     private float movementInputAcceleration;
@@ -66,8 +94,10 @@ public class PlayerMovementClimb : MonoBehaviour
         playerRigidbody = GetComponent<Rigidbody>();
         playerCollider = GetComponent<CapsuleCollider>();
 
+        // Initialize variables
         state = animator.GetCurrentAnimatorStateInfo(0);
         nextState = animator.GetNextAnimatorStateInfo(0);
+        inStateTransition = animator.IsInTransition(0);
 
         targetPosition = transform.position;
         targetAngle = transform.eulerAngles.y;
@@ -76,29 +106,39 @@ public class PlayerMovementClimb : MonoBehaviour
 
     private void FixedUpdate()
     {
+        // Update state variables
         state = animator.GetCurrentAnimatorStateInfo(0);
         nextState = animator.GetNextAnimatorStateInfo(0);
         inStateTransition = animator.IsInTransition(0);
 
-        // Moving speed
+        // Calculate current movement speed
+        // It will gradually decrease/increase, so the animations and movement are smoother
         movementInputSpeed = Mathf.SmoothDamp(movementInputSpeed,
             state.fullPathHash == State.Moving && !inStateTransition || nextState.fullPathHash == State.Moving ?
                 movementInput.magnitude :
                 0,
             ref movementInputAcceleration, movementInputAccelerationTime);
 
+        // Update movement speed on animator to adjust animation
         animator.SetFloat(AnimatorParameters.Movement, movementInputSpeed);
 
-        // Rotation
+        // Set rotation value
+        // Do not change rotation when transitioning to Hanging state,
+        // to avoid wrong orientation when hanging on ledge
         if (state.fullPathHash == State.Moving && nextState.fullPathHash != State.Hanging && movementInput.magnitude >= 0.1f)
-            targetAngle = Mathf.Atan2(movementInput.x, movementInput.y) * Mathf.Rad2Deg + cameraTransform.eulerAngles.y;
+            targetAngle = Mathf.Atan2(movementInput.x, movementInput.y) * Mathf.Rad2Deg + playerCamera.eulerAngles.y;
 
         if (state.fullPathHash == State.Climbing)
         {
+            // Adjust collider height during climbing animation, according to the animation curves
+            // The obtained value from the curves goes from 0 to 1, 1 being full height, 0 being no height
             float newColliderHeight;
 
             if (animator.IsInTransition(0))
             {
+                // During a transition, gradually adjust collider to full length
+                // Relative to the transition's length
+
                 if (colliderDiff == -1)
                 {
                     colliderDiff = defaultColliderHeight - playerCollider.height;
@@ -124,6 +164,8 @@ public class PlayerMovementClimb : MonoBehaviour
             float diff = playerCollider.height - newColliderHeight;
             playerCollider.height = newColliderHeight;
 
+            // The collider is moved, so the top of the collider
+            // is always in the same spot relative to the character
             Vector3 collCenter = playerCollider.center;
             collCenter.y += diff / 2;
             playerCollider.center = collCenter;
@@ -155,36 +197,62 @@ public class PlayerMovementClimb : MonoBehaviour
         }
     }
 
+    /// <summary>
+    /// Returns the current state of the character.
+    /// </summary>
+    /// <returns>Current state of the character.</returns>
     public AnimatorStateInfo GetState()
     {
         return state;
     }
 
+    /// <summary>
+    /// Set new position for the character (change will be gradual).
+    /// </summary>
+    /// <param name="position">New position.</param>
     public void SetPosition(Vector3 position)
     {
         targetPosition = position;
         translate = true;
     }
 
+    /// <summary>
+    /// Apply translation to character (change will be gradual).
+    /// </summary>
+    /// <param name="translation">Translation to be applied.</param>
     public void ApplyTranslation(Vector3 translation)
     {
         targetPosition = transform.position + translation;
         translate = true;
     }
 
+    /// <summary>
+    /// Set new orientation for the character (change will be gradual).
+    /// </summary>
+    /// <param name="angle">New orientation.</param>
     public void SetAngle(float angle)
     {
         targetAngle = angle;
     }
 
+    /// <summary>
+    /// Apply rotation to character (change will be gradual).
+    /// </summary>
+    /// <param name="rotation">Rotation to be applied.</param>
     public void ApplyRotation(float rotation)
     {
         targetAngle = transform.eulerAngles.y + rotation;
     }
 
+    /// <summary>
+    /// Move the character to the target position gradually.
+    /// </summary>
     private void Move()
     {
-
+        // Movement will only be applied if translate flag is set to true
+        // This flag is only true after the targetPosition has been changed
+        // and will be set to false after the targetPosition is reached
+        // This way, root motion is not affected unintentionally.
         if (!translate)
             return;
 
@@ -198,6 +266,9 @@ public class PlayerMovementClimb : MonoBehaviour
         transform.position = position;
     }
 
+    /// <summary>
+    /// Rotate the character to the target orientation gradually.
+    /// </summary>
     private void Rotate()
     {
         if (Mathf.Abs(transform.eulerAngles.y - targetAngle) < 0.001)
@@ -207,8 +278,15 @@ public class PlayerMovementClimb : MonoBehaviour
         transform.rotation = Quaternion.Euler(0f, angle, 0f);
     }
 
+    /// <summary>
+    /// Hang character on ledge.
+    /// </summary>
+    /// <param name="position">Central position of the ledge.</param>
+    /// <param name="orientation">Orientation of the ledge (to where the character will face).</param>
     public void HangOnLedge(Vector3 position, Quaternion orientation)
     {
+        // Only valid if character is in Moving state
+        // and not already transitioning to Hanging state
         if (state.fullPathHash != State.Moving)
             return;
 
@@ -216,13 +294,26 @@ public class PlayerMovementClimb : MonoBehaviour
             return;
 
         animator.SetTrigger(AnimatorParameters.Hang);
+
+        // Force update animator to start state transition immediately 
+        // deltaTime is 0 so animations are not affected
+        // CONFIRM THIS - https://forum.unity.com/threads/question-about-animator-force-update-with-deltatime-0.1094890/
+        //
+        // This fixes an issue with FixedUpdate running after trigger is set,
+        // but before transition to Hanging state starts
         animator.Update(0f);
 
+        // Character is temporarily rotated to target orientation
+        // to calculate necessary translation relative to the final hand position
         Quaternion oldRotation = transform.rotation;
         transform.rotation = orientation;
 
+        // Vector from hands to center of ledge
         Vector3 diffPosition = position - hangingPoint.position;
 
+        // Get character's forward and up vectors and project them to diffPosition's length
+        // This will result in the necessary translation to put the character on the ledge,
+        // without moving him sideways along it
         Vector3 translationVectorForward = transform.forward;
         translationVectorForward = Vector3.Project(diffPosition, translationVectorForward);
 
@@ -231,28 +322,34 @@ public class PlayerMovementClimb : MonoBehaviour
 
         Vector3 translationVector = translationVectorForward + translationVectorUp;
 
+        // Reset position
         transform.rotation = oldRotation;
 
-        // Move character a total of translationVector
         ApplyTranslation(translationVector);
         SetAngle(orientation.eulerAngles.y);
     }
 
-    #region Input event methods
+    /// <summary>
+    /// Consumes movement input.
+    /// </summary>
+    /// <param name="context">Input callback context.</param>
     private void CaptureMovementDirection(InputAction.CallbackContext context)
     {
         movementInput = context.ReadValue<Vector2>();
     }
 
+    /// <summary>
+    /// Consumes climb trigger input.
+    /// </summary>
+    /// <param name="context">Input callback context.</param>
     private void TriggerClimb(InputAction.CallbackContext context)
     {
-        // Will only trigger climb if current state is hanging
+        // Will only trigger climb if current state is Hanging
         if (state.fullPathHash == State.Hanging)
         {
             animator.SetTrigger(AnimatorParameters.Climb);
         }
     }
-    #endregion
 
     private void OnEnable()
     {
