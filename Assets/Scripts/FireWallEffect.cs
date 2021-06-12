b56375599b47faed539fedac50c17795547c1fb1
paulom174 <paulomoutinho174@gmail.com>
2021-06-12 22:44:32 +0100

    feat: fire wall now does dmg to enemies

---
+++ Assets/Scripts/FireWallEffect.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireWallEffect : MonoBehaviour
+{
+    public int damage = 500;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // Deal damage to enemy
+        if (other.CompareTag("Enemy"))
+        {
+            CharacterStats enemyStats = other.GetComponent<CharacterStats>();
+
+            if (enemyStats)
+                enemyStats.TakeDamage(damage);
+        }
+    }
+}
