6da24b92923520c4ca39684ff90a8396cc91370c
jfcaraujo <jfcaraujo99@gmail.com>
2021-06-16 16:57:58 +0100

    Finished Main Menu

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    ///<summary>
+    /// Loads up the main scene in the selected difficulty
+    ///</summary>
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("Scenes/LevelOne");
+    }
+
+    ///<summary>
+    /// Quits the game
+    ///</summary>
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void ButtonSound()
+    {
+        FMODUnity.RuntimeManager.PlayOneShot("event/:Click");
+    }
+}
