afd6882e1bb3c19757d5f77130f84d014be2aae2
Filipe Barbosa <filipe.g.barbosa@gmail.com>
2021-06-16 22:58:24 +0100

    Add death sound to characters

--- Assets/Scripts/DodgeState.cs
+++ Assets/Scripts/DodgeState.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DeathState : StateMachineBehaviour
+public class DodgeState : StateMachineBehaviour
 {
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
