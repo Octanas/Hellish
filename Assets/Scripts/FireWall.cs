4f04726c950e6db017e4c53a075963b473be381e
Filipe Barbosa <filipe.g.barbosa@gmail.com>
2021-06-16 23:34:14 +0100

    Add part of fire wall sound (sword part)

--- Assets/Scripts/FireWall.cs
+++ Assets/Scripts/FireWall.cs
@@ -36,30 +36,35 @@ public class FireWall : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (walled) {
+        if (walled)
+        {
             Vector3 wallScale = wallObject.transform.localScale;
             Vector3 pos = wallObject.transform.forward * wallObject.transform.localScale.z + wallObject.transform.position;
             // Debug.DrawRay(pos, Vector3.up, Color.green);
             Vector3 groundRay = new Vector3(pos.x, pos.y + 1f, pos.z);
             RaycastHit ground;
 
-            if(!Physics.Raycast(groundRay, Vector3.down, out ground, 1) && wallLength == maxWallLength) {
+            if (!Physics.Raycast(groundRay, Vector3.down, out ground, 1) && wallLength == maxWallLength)
+            {
                 wallLength = Vector3.Distance(groundRay, transform.position) - 1f;
             }
-            else if(Physics.Raycast(groundRay, wallObject.transform.forward, out ground, 0.1f) && wallLength == maxWallLength && ground.collider != null && ground.collider.tag != "Enemy") {
+            else if (Physics.Raycast(groundRay, wallObject.transform.forward, out ground, 0.1f) && wallLength == maxWallLength && ground.collider != null && ground.collider.tag != "Enemy")
+            {
                 wallLength = Vector3.Distance(groundRay, transform.position);
             }
 
             Debug.DrawRay(groundRay, Vector3.down, Color.green);
             Debug.DrawRay(groundRay, wallObject.transform.forward * 0.1f, Color.red);
-            
+
             Vector3 targetScale = Vector3.Lerp(wallScale, new Vector3(wallScale.x, maxWallHeight, wallLength), Time.deltaTime * 2);
             wallObject.transform.localScale = targetScale;
         }
     }
 
-    private void Wall (InputAction.CallbackContext context) {
-        if(playerAttack.hasSword && !walled && playerStats.CheckWall()) {
+    private void Wall(InputAction.CallbackContext context)
+    {
+        if (playerAttack.hasSword && !walled && playerStats.CheckWall())
+        {
             animator.SetTrigger("FireWall");
         }
     }
@@ -67,7 +72,8 @@ public class FireWall : MonoBehaviour
 
     private void WallEvent()
     {
-        if (!walled) {
+        if (!walled)
+        {
             wallObject = Instantiate(wallPrefab, transform.position + transform.forward * 0.5f + Vector3.up * -0.4f, transform.rotation);
             walled = true;
             coroutine = cooldown(cooldownTime);
@@ -75,8 +81,13 @@ public class FireWall : MonoBehaviour
         }
     }
 
-     private IEnumerator cooldown(float waitTime)
-    { 
+    private void PlaySwordSound()
+    {
+        FMODUnity.RuntimeManager.PlayOneShotAttached("event:/Player/Sword/Sword Swing", gameObject);
+    }
+
+    private IEnumerator cooldown(float waitTime)
+    {
         yield return new WaitForSeconds(waitTime);
         Object.Destroy(wallObject);
         walled = false;
