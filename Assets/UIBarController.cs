00a7502f80a7177876c7a6fcbc173acf96c601ba
paulom174 <paulomoutinho174@gmail.com>
2021-06-17 18:21:39 +0100

    feat: added mana bar

---
+++ Assets/UIBarController.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIBarController : MonoBehaviour
+{
+    public Transform healthTransform;
+    public float initialHealth;
+    private Vector3 targetHealth;
+    public Transform manaTransform;
+    public float initialMana;
+    private Vector3 targetMana;
+
+    private PlayerStats stats;
+    void Start()
+    {
+        stats = GetComponent<PlayerStats>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        targetHealth = Vector3.Lerp(healthTransform.localScale, new Vector3(stats.maxHealth/initialHealth, healthTransform.localScale.y, healthTransform.localScale.z), Time.deltaTime * 2);
+
+        healthTransform.localScale = targetHealth;
+
+        targetMana = Vector3.Lerp(manaTransform.localScale, new Vector3(stats.maxMana/initialMana, manaTransform.localScale.y, manaTransform.localScale.z), Time.deltaTime * 2);
+
+        manaTransform.localScale = targetMana;
+    }
+}
