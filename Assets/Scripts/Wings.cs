c673a8ddc2ebc0d98b7c6ade75ac6a1c35df1b6a
Filipe Barbosa <filipe.g.barbosa@gmail.com>
2021-06-17 16:14:33 +0100

    Add wing sounds to flying enemy

--- Assets/Scripts/Wings.cs
+++ Assets/Scripts/Wings.cs
@@ -3,37 +3,42 @@ using UnityEngine;
 
 public class Wings : MonoBehaviour
 {
-    //left  y 0 70 | x 180 160,
-    //right y 0 -70 | x 0  20 
+    //left  y 0 90 | x 180 160,
+    //right y 0 -90 | x 0  20 
     public Transform leftWing, rightWing, baseLeftWing, baseRightWing;
     private const int min = 0, max = 90;
+    private const int minBase = 0, maxBase = 20;
     private float direction = 1f;
-    private int increment, totalTicks = 15;
+    private float increment, incrementBase;
+    private float totalSeconds = 0.3f;
 
     void Start()
     {
-        increment = (max - min) / totalTicks;
+        increment = (max - min) / totalSeconds * Time.fixedDeltaTime;
+        incrementBase = (maxBase - minBase) / totalSeconds * Time.fixedDeltaTime;
     }
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         var rotationLeft = leftWing.localEulerAngles;
         var rotationRight = rightWing.localEulerAngles;
         var rotationBaseLeft = baseLeftWing.localEulerAngles;
         var rotationBaseRight = baseRightWing.localEulerAngles;
+
         if (Math.Abs(rotationLeft.y - max) <= 1)
         {
+            FMODUnity.RuntimeManager.PlayOneShotAttached("event:/Player/Jump/wing_flap", gameObject);
             direction = -1;
         }
         else if (Math.Abs(rotationLeft.y - min) <= 1)
             direction = 1;
 
-        leftWing.localEulerAngles = new Vector3(rotationLeft.x, rotationLeft.y + direction * increment, rotationLeft.z);
-        rightWing.localEulerAngles = new Vector3(rotationRight.x, rotationRight.y - direction * increment, rotationRight.z);
-        baseLeftWing.localEulerAngles = new Vector3((float) (rotationBaseLeft.x - direction * 20.0 / totalTicks),
-            rotationBaseLeft.y, rotationBaseLeft.z);
-        baseRightWing.localEulerAngles = new Vector3((float) (rotationBaseRight.x + direction * 20.0 / totalTicks),
-            rotationBaseRight.y, rotationBaseRight.z);
+        float wingIncrement = direction * increment;
+        float baseWingIncrement = direction * incrementBase;
+
+        leftWing.localEulerAngles = new Vector3(rotationLeft.x, rotationLeft.y + wingIncrement, rotationLeft.z);
+        rightWing.localEulerAngles = new Vector3(rotationRight.x, rotationRight.y - wingIncrement, rotationRight.z);
+        baseLeftWing.localEulerAngles = new Vector3(rotationBaseLeft.x - baseWingIncrement, rotationBaseLeft.y, rotationBaseLeft.z);
+        baseRightWing.localEulerAngles = new Vector3(rotationBaseRight.x + baseWingIncrement, rotationBaseRight.y, rotationBaseRight.z);
     }
 }
